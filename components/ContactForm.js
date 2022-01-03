897a5a02211bb68e18645b6cf0c6e1fb7b9e8c77
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-03 15:20:41 -0700

    refactor

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -60,114 +60,127 @@ const ContactForm = () => {
   }
 
   return ( 
-    <>
-      <form 
-        onSubmit={handleSubmit}
-        name="contactform" 
-        method="POST" 
-        netlify-honeypot="honeyjar"
-        data-netlify="true" 
-        className={clsx("grid content-start", {"display-invalid" : highlightInvalidFields})}
-      >
-        <input 
-          type="hidden" 
-          name="form-name"
-          value="contactform" 
-        />
+    <form 
+      onSubmit={handleSubmit}
+      name="contactform" 
+      method="POST" 
+      netlify-honeypot="honeyjar"
+      data-netlify="true" 
+      className={clsx("grid content-start", {"display-invalid" : highlightInvalidFields})}
+    >
+      {/* netlify forms */}
+      <input 
+        type="hidden" 
+        name="form-name"
+        value="contactform" 
+      />
 
-        <div className="hidden">
-          <label>
-            <input 
-              type="text" 
-              name="honeyjar" 
-            />
-          </label>
-        </div>
-
-        <div className="grid mb-3">
-          <label 
-            htmlFor="name" 
-            className="font-bold mb-1">
-            Name
-          </label>
+      {/* honeypot */}
+      <div className="hidden">
+        <label>
           <input 
             type="text" 
-            id="name"
-            name="name" 
-            placeholder="Name"
-            required
-            autoComplete="off"
-            className="input"
+            name="honeyjar" 
           />
-          <span className="input-hint">Name is required.</span>
-        </div>
+        </label>
+      </div>
 
-        <div className="grid mb-3">
-          <label 
-            htmlFor="email"
-            className="font-bold mb-1"
-          >
-            Email
-          </label>
-          <input 
-            type="email" 
-            id="email"
-            name="email" 
-            placeholder="Email"
-            required
-            autoComplete="email"
-            className="input"
-          />
-          <span className="input-hint">Valid email is required.</span>
-        </div>
-
-        <div className="grid mb-3">
-          <label 
-            htmlFor="message" 
-            className="font-bold mb-1"
-          >
-            Message
-          </label>
-          <textarea 
-            id="message" 
-            name="message" 
-            placeholder="Message"
-            required
-            className="input min-h-[100px] max-h-[300px]"
-          />
-          <span className="input-hint">Message is required.</span>
-        </div>
-
-        <div className="grid mb-8">
-          <span className="font-bold mb-1">Captcha</span>
-          <div className="relative w-full overflow-auto h-[60px] mobile-xl:h-full">
-            <div className="absolute transform scale-[0.75] origin-top-left mobile-xl:static mobile-xl:scale-100">
-              <Reaptcha
-                sitekey={captchaKey}
-                onVerify={(token) => setCaptchaResponse(token)}
-                onError={() => setCaptchaHint('Captcha Unavailable.')}
-                ref={captchaRef}
-              />
-            </div>
-          </div>
-          
-          { captchaHint && <span className="text-xs text-red-500">{captchaHint}</span> }
-          <p 
-            onClick={refreshCaptcha}
-            className="mt-1 text-xs text-primary cursor-pointer justify-self-start border-transparent border-b-2 transition-colors hover:border-primary"
-          >
-            Not loading? Click here to refresh.
-          </p>
-        </div>
-
-        <button 
-          type="submit"
-          className="button w-40 justify-self-center"
+      <div className="grid mb-3">
+        <label 
+          htmlFor="name" 
+          className="font-bold mb-1"
         >
-          Send
-        </button>
-      </form>
-    </>
+          Name
+        </label>
+        <input 
+          type="text" 
+          id="name"
+          name="name" 
+          placeholder="Name"
+          required
+          autoComplete="off"
+          className="input"
+        />
+        <span className="input-hint">
+          Name is required.
+        </span>
+      </div>
+
+      <div className="grid mb-3">
+        <label 
+          htmlFor="email"
+          className="font-bold mb-1"
+        >
+          Email
+        </label>
+        <input 
+          type="email" 
+          id="email"
+          name="email" 
+          placeholder="Email"
+          required
+          autoComplete="email"
+          className="input"
+        />
+        <span className="input-hint">
+          Valid email is required.
+        </span>
+      </div>
+
+      <div className="grid mb-3">
+        <label 
+          htmlFor="message" 
+          className="font-bold mb-1"
+        >
+          Message
+        </label>
+        <textarea 
+          id="message" 
+          name="message" 
+          placeholder="Message"
+          required
+          className="input min-h-[100px] max-h-[300px]"
+        />
+        <span className="input-hint">
+          Message is required.
+        </span>
+      </div>
+
+      <div className="grid mb-8">
+        <span className="font-bold mb-1">
+          Captcha
+        </span>
+        <div className="relative w-full overflow-auto h-[60px] mobile-xl:h-full">
+          <div className="absolute transform scale-[0.75] origin-top-left mobile-xl:static mobile-xl:scale-100">
+            <Reaptcha
+              sitekey={captchaKey}
+              onVerify={(token) => setCaptchaResponse(token)}
+              onError={() => setCaptchaHint('Captcha Unavailable.')}
+              ref={captchaRef}
+            />
+          </div>
+        </div>
+        
+        { captchaHint && (
+          <span className="text-xs text-red-500">
+            {captchaHint}
+          </span> 
+        )}
+        <p 
+          onClick={refreshCaptcha}
+          className="mt-1 text-xs text-primary cursor-pointer justify-self-start border-transparent border-b-2 transition-colors hover:border-primary"
+        >
+          Not loading? Click here to refresh.
+        </p>
+      </div>
+
+      <button 
+        type="submit"
+        className="button w-40 justify-self-center"
+      >
+        Send
+      </button>
+    </form>
   );
 }
  
