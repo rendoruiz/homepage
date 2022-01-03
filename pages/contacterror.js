e0d577aa84dae6606ccf5a3da229dd18de8b0abd
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-03 16:32:02 -0700

    update contact error

--- pages/contacterror.js
+++ pages/contacterror.js
@@ -1,19 +1,24 @@
-import Link from 'next/link'
-
-import SiteLayout from "../components/SiteLayout";
+import ContactResponseLayout from '../components/Contact/ContactResponse/ContactResponseLayout';
 
 const ContactError = () => {
   return ( 
-    <SiteLayout>
-      <div className="h-full grid justify-center content-center text-center">
-        <h2 className="text-5xl font-bold text-red-500 md:text-6xl">Uh oh!</h2>
-        <span className="mt-1 text-2xl text-gray-500">An error has occured.</span>
-        <span className="mt-4 text-gray-500">Try sending your message again later.</span>
-        <Link href="/" replace>
-          <a className="button mt-7 mb-[-20px] w-40 justify-self-center md:mt-9 md:mb-0">Go back</a>
-        </Link>
-      </div>
-    </SiteLayout>
+    <ContactResponseLayout title="Sending Failed">
+      <span 
+        className="text-7xl drop-shadow-md sm:text-8xl"
+        title="knocked-up face emoji"
+      >
+        😵
+      </span>
+      <h2 className="mt-3 text-3xl font-bold text-primary tracking-wide sm:mt-5 sm:text-4xl">
+        Uh oh!
+      </h2>
+      <p className="mt-1 text-gray-500 sm:text-lg">
+        An error has occured.
+      </p>
+      <p className="mt-2 text-xs text-gray-500/80 sm:text-sm">
+        Try sending your message again in a bit.
+      </p>
+    </ContactResponseLayout>
   );
 }
  
