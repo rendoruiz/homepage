19145a9fd966947d39713b57e2b1c7bda0494597
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-03 17:32:03 -0700

    update scrolltoview trigger style

--- components/Spotlight/Spotlight.js
+++ components/Spotlight/Spotlight.js
@@ -60,7 +60,7 @@ const ScrollTrigger = ({ anchor, displayText }) => {
 
   return (
     <span
-      className="font-bold text-primary cursor-pointer" 
+      className="relative font-bold text-primary cursor-pointer sm:hover:after:bg-primary sm:hover:after:scale-x-100 sm:after:absolute sm:after:inset-x-0 sm:after:bottom-0 sm:after:rounded-full sm:after:mx-auto sm:after:h-1 sm:after:w-full sm:after:bg-primary/40 sm:after:scale-x-[0.25] sm:after:transition sm:after:ease-out sm:after:duration-200" 
       onClick={() => scrollToAnchor(anchor)}
     >
       { displayText }
