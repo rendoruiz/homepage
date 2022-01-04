a291c52761dd75a24af55f759f2f8ca4a3c55726
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-03 22:22:29 -0700

    align button styles

--- components/Contact/ContactResponseLayout.js
+++ components/Contact/ContactResponseLayout.js
@@ -16,7 +16,9 @@ const ContactResponseLayout = ({ title, children }) => {
 
         {/* back to home */}
         <Link href="/" replace>
-          <a className="button mt-7 mb-[-20px] w-40 justify-self-center text-sm sm:mt-9 sm:mb-0">Go back</a>
+          <a className="justify-self-center border-2 border-primary rounded-full mt-7 mb-[-20px] px-10 py-1 font-bold text-gray-800 text-center uppercase transition hover:bg-primary/80 hover:text-white active:opacity-80">
+            Go back
+          </a>
         </Link>
       </div>
     </SiteLayout>
