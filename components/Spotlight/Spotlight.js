b9a24bae1a59cfd2cea0f4300cc4d024d5888860
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-03 17:18:10 -0700

    refactor

--- components/Spotlight/Spotlight.js
+++ components/Spotlight/Spotlight.js
@@ -2,9 +2,47 @@ import { useRouter } from "next/router";
 import { scroller } from 'react-scroll'
 
 const Spotlight = () => {
+  return (  
+    <section className="px-5">
+      <div className="mx-auto max-w-screen-sm w-full py-20">
+        <h1 className="text-4xl font-bold text-gray-900">
+          Hello, I'm Rendo.
+        </h1>
+        <p className="text-gray-500 text-xl font-bold">
+          I do front-end stuff.
+        </p>
+          
+        <div className="grid gap-4 mt-7 leading-loose">
+          <p>
+            I am the front-end developer lead at BridgeIT Tech Solutions. I graduated last year from NAIT under the Digital Media & IT program in the Computer Software Development concentration. I enjoy working on front-end and also on the back-end when needed.
+          </p>
+          <p>
+            My current toolset and techstack includes: HTML, CSS, JavaScript, PHP, Tailwind CSS, Laravel, Alpine.js, React, Next.js, Git, GitHub, Visual Studio Code.
+          </p>
+
+          <p>
+            <span>Scroll down for </span>
+            <ScrollTrigger 
+              anchor="portfolio"
+              displayText="my portfolio"
+            />,
+            <br />
+            <span>and a little bit more for </span>
+            <ScrollTrigger 
+              anchor="contact"
+              displayText="my contact"
+            />.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+const ScrollTrigger = ({ anchor, displayText }) => {
   const router = useRouter();
 
-  const anchorScrollToView = (id) => {
+  const scrollToAnchor = (id) => {
     const scrollDuration = 800;
 
     // scroll to element
@@ -20,19 +58,13 @@ const Spotlight = () => {
     }, scrollDuration);
   }
 
-  return (  
-    <section className="px-5">
-      <div className="mx-auto max-w-screen-sm w-full py-20">
-        <h1 className="text-4xl font-bold text-gray-900">Hello, I'm Rendo.</h1>
-        <span className="text-gray-500 text-xl font-bold">Web Developer</span>
-        <p className="mt-7 leading-7">
-          I am a recent graduate from NAIT under the Digital Media & IT program in the Computer Software Development concentration. I enjoy working on front-end web development and have experience building sites using React.js, Next.js, Create React App, Tailwind CSS, HTML, CSS, and JavaScript.
-        </p>
-        <p className="mt-5 leading-7">
-          Scroll down to check out <span className="anchorlink" onClick={() => anchorScrollToView("portfolio")}>my portfolio</span>, and feel free to <span className="anchorlink" onClick={() => anchorScrollToView("contact")}>leave a message</span> on the contact form further below.
-        </p>
-      </div>
-    </section>
+  return (
+    <span
+      className="font-bold text-primary cursor-pointer" 
+      onClick={() => scrollToAnchor(anchor)}
+    >
+      { displayText }
+    </span>
   );
 }
  
